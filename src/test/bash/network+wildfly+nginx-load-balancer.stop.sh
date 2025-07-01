cd16f15608ac34452981d0a546546dff56eacc48
Reşat SABIQ <haqer@gmx.fr>
2025-07-01 20:00:02 +0200

    Add script to stop & remove Pure-Docker containers

---
+++ src/test/bash/network+wildfly+nginx-load-balancer.stop.sh
@@ -0,0 +1,10 @@
+for c in $(docker container ls -q); do
+	image=$(docker inspect $c --format "{{.Config.Image}}")
+	if [[ $image =~ wildfly-nginx ]] || [[ $image =~ jboss-cluster-* ]]; then
+		echo -n "$image "
+		docker container stop $c
+		docker container rm $c
+	else
+		echo "	(doesn't match, skipped: $c)"
+	fi
+done
