3fc115eeafdcb47aa8e2f4b8aca3bbe68a41262e
Reşat SABIQ <haqer@gmx.fr>
2025-07-01 09:42:54 +0200

    Use helm-based service directly in sticky-sessions.yaml & further reduce delays between requests
     * confirmed to work based on 15 exec.s of StickySessionsTest
       - with .1 for several last ones

--- src/test/bash/cluster-test-template.sh
+++ src/test/bash/cluster-test-template.sh
@@ -7,7 +7,7 @@ fi
 mkdir -p target/log/cookies
 
 src/test/bash/curl.init-session.0.sh $execId $website
-sleep .25 # just in case: for cookie file persistance, etc.
+sleep .1 # just in case: for cookie file persistance, etc. (+ there would be delays between user requests)
 src/test/bash/put+get/curl.put.1.sh $execId $website
-sleep .25
+sleep .1
 src/test/bash/put+get/curl.get.loop.2.sh $execId $website $request_count
