63d661781bac61df91f3b9c9d15752431088b02a
Reşat SABIQ <haqer@gmx.fr>
2025-07-01 14:04:30 +0200

    Minor refactoring in NoStickySessionNoReplicationTest

--- src/test/java/resat/sabiq/jboss/cluster/hi/availability/demo/NoStickySessionNoReplicationTest.java
+++ src/test/java/resat/sabiq/jboss/cluster/hi/availability/demo/NoStickySessionNoReplicationTest.java
@@ -11,15 +11,16 @@ import org.junit.jupiter.api.Test;
  * @author	Reşat SABIQ
  */
 class NoStickySessionNoReplicationTest extends AbstractTestTemplate {
+	private static String DATA_PRESENCE_ERR_MSG = """
+			With no sticky sessions & no replication sometimes data is expected to not be
+			there on the instance called.""";
 	@Test
-	void givenSessionWithData_WhenNumerousRequests_someRequestsDontHaveSessionData()
+	void givenSessionWithData_whenNumerousRequests_someRequestsDontHaveSessionData()
 			throws IOException, InterruptedException, ExecutionException {
 		final int matchesCount
 			= execTestScriptAndReturnMatchesCount("no-sticky-sessions-no-replication-test.sh");
 		System.out.printf("Success rate < 100%% as expected for %d requests: %s%n", GET_REQUEST_COUNT,
 				percentFormat.format(toSuccessRatio(matchesCount)));
-		assertTrue(matchesCount < GET_REQUEST_COUNT,
-				"With no sticky sessions & no replication sometimes data is expected to not be "
-						+ "there on the instance called.");
+		assertTrue(matchesCount < GET_REQUEST_COUNT, DATA_PRESENCE_ERR_MSG);
 	}
 }
