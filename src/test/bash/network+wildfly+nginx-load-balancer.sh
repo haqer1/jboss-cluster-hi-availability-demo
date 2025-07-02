033d028452c011e03e4e4f1d55ee01a9f7491306
Reşat SABIQ <haqer@gmx.fr>
2025-07-02 02:13:17 +0200

    Use standalone-ha config in network+wildfly+nginx-load-balancer.sh

--- src/test/bash/network+wildfly+nginx-load-balancer.sh
+++ src/test/bash/network+wildfly+nginx-load-balancer.sh
@@ -11,16 +11,16 @@ docker run -d --name wild1 -h wild1 -p 8090:8080 -p 9990:9990 --network=wildnetw
 	--ip 172.28.5.2 $artifact /bin/bash -c 'ip=$(awk '"'"'END{print $1}'"'"' /etc/hosts) && \
 		/opt/jboss/wildfly/bin/standalone.sh -Djboss.bind.address.management=$ip \
 			-Djboss.bind.address=$ip -Djboss.bind.address.private=$ip \
-			-Djboss.bind.address.unsecure=$ip -c standalone-full-ha.xml -u 230.0.0.4'
+			-Djboss.bind.address.unsecure=$ip -c standalone-ha.xml -u 230.0.0.4'
 docker run -d --name wild2 -h wild2 -p 8091:8080 -p 9991:9990 --network=wildnetwork \
 	--ip 172.28.5.3 $artifact /bin/bash -c 'ip=$(awk '"'"'END{print $1}'"'"' /etc/hosts) && \
 		/opt/jboss/wildfly/bin/standalone.sh -Djboss.bind.address.management=$ip \
 			-Djboss.bind.address=$ip -Djboss.bind.address.private=$ip \
-			-Djboss.bind.address.unsecure=$ip -c standalone-full-ha.xml -u 230.0.0.4'
+			-Djboss.bind.address.unsecure=$ip -c standalone-ha.xml -u 230.0.0.4'
 docker run -d --name wild3 -h wild3 -p 8092:8080 -p 9992:9990 --network=wildnetwork \
 	--ip 172.28.5.4 $artifact /bin/bash -c 'ip=$(awk '"'"'END{print $1}'"'"' /etc/hosts) && \
 		/opt/jboss/wildfly/bin/standalone.sh -Djboss.bind.address.management=$ip \
 			-Djboss.bind.address=$ip -Djboss.bind.address.private=$ip \
-			-Djboss.bind.address.unsecure=$ip -c standalone-full-ha.xml -u 230.0.0.4'
+			-Djboss.bind.address.unsecure=$ip -c standalone-ha.xml -u 230.0.0.4'
 
 docker run -p 172.17.0.1:80:80 --network=wildnetwork --ip 172.28.5.5 wildfly-nginx:0.3
