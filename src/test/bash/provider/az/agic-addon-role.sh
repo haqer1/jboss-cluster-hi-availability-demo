498673cf49fce42778609b69b3b6153e2c89f4b0
Reşat SABIQ <haqer@gmx.fr>
2025-07-10 16:54:19 +0200

    AKS as example of public K8s service: Add agic-addon-role.sh
     * according to documentation this is required for AGIC config

---
+++ src/test/bash/provider/az/agic-addon-role.sh
@@ -0,0 +1,15 @@
+# Get application gateway id from AKS addon profile
+appGatewayId=$(az aks show -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP_NAME -o tsv \
+	--query "addonProfiles.ingressApplicationGateway.config.effectiveApplicationGatewayId")
+
+# Get Application Gateway subnet id
+appGatewaySubnetId=$(az network application-gateway show --ids $appGatewayId -o tsv \
+	--query "gatewayIPConfigurations[0].subnet.id")
+
+# Get AGIC addon identity
+agicAddonIdentity=$(az aks show -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP_NAME -o tsv \
+	--query "addonProfiles.ingressApplicationGateway.identity.clientId")
+
+# Assign network contributor role to AGIC addon ID to subnet that contains the Application Gateway
+az role assignment create --assignee $agicAddonIdentity --scope $appGatewaySubnetId \
+	--role "Network Contributor"
