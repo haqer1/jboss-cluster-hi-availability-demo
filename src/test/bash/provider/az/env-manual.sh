33f26ae35605b7aa3dec07fd5e3c90169d11bb5b
Reşat SABIQ <haqer@gmx.fr>
2025-07-10 16:34:49 +0200

    AKS as example of public K8s service: Add env-manual.sh & env-print.sh
     * the former might be required to ensure AMD64 arch
     * the latter to easily see currently set cluster-related env. variables

---
+++ src/test/bash/provider/az/env-manual.sh
@@ -0,0 +1,7 @@
+export RANDOM_ID="4"
+export DEMO=
+export RESOURCE_GROUP_NAME="${DEMO}RG$RANDOM_ID"
+export REGION="francecentral"
+export AKS_CLUSTER_NAME="${DEMO}C$RANDOM_ID"
+export APP_GATEWAY_NAME=${DEMO}Gateway
+export DNS_LABEL="jboss-replication-ha-demo"
