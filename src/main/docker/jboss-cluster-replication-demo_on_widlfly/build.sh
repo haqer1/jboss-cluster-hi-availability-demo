e9e4053c4c016380edb68c9d4256dc1f5d9da48f
Reşat SABIQ <haqer@gmx.fr>
2025-07-10 22:07:08 +0200

    Update s/m/d/jboss-cluster-replication-demo_on_widlfly/build.sh to use Kube-ping-based version

--- src/main/docker/jboss-cluster-replication-demo_on_widlfly/build.sh
+++ src/main/docker/jboss-cluster-replication-demo_on_widlfly/build.sh
@@ -1,2 +1,2 @@
 docker build -f src/main/docker/jboss-cluster-replication-demo_on_widlfly/Dockerfile \
-	--tag jboss-cluster-hi-availability-demo:0.8 --rm=true .
+	--tag jboss-cluster-hi-availability-demo:0.9 --rm=true .
